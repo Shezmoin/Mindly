d50cc49567485839b8e81af5b55cb1bb444ffe45
Shezmoin <shezmoin@hotmail.com>
2026-04-02 13:36:32 +0100

    feat: add UserProfile model with post_save signal for auto-creation

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, UserProfile
 
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subscription_tier', 'joined_date', 'reminder_time')
+    list_filter = ('subscription_tier', 'joined_date')
+    search_fields = ('user__username', 'user__email')
--- users/models.py
+++ users/models.py
@@ -1,6 +1,32 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 
 
 class CustomUser(AbstractUser):
-	bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(blank=True, null=True)
+
+
+class UserProfile(models.Model):
+    TIER_FREE = 'free'
+    TIER_PREMIUM = 'premium'
+    SUBSCRIPTION_CHOICES = [
+        (TIER_FREE, 'Free'),
+        (TIER_PREMIUM, 'Premium'),
+    ]
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    subscription_tier = models.CharField(max_length=10, choices=SUBSCRIPTION_CHOICES, default=TIER_FREE)
+    joined_date = models.DateField(auto_now_add=True)
+    reminder_time = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} profile"
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
