a6ee4d0de2058e93fdfc74e59faa7acec722c3e9
Shezmoin <shezmoin@hotmail.com>
2026-04-02 16:11:31 +0100

    fixed issue with the broken form links

--- mindly/settings.py
+++ mindly/settings.py
@@ -142,6 +142,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom user model
 AUTH_USER_MODEL = 'users.CustomUser'
 
+# Authentication redirects
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
