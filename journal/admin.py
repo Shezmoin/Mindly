aff597eb933d841a17160fa6d10f838d7e9322e2
Shezmoin <shezmoin@hotmail.com>
2026-04-02 13:39:05 +0100

    feat: add MoodEntry and JournalEntry models in journal app

--- journal/admin.py
+++ journal/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import JournalEntry, MoodEntry
+
+
+@admin.register(MoodEntry)
+class MoodEntryAdmin(admin.ModelAdmin):
+	list_display = ('user', 'mood_score', 'created_at')
+	list_filter = ('mood_score', 'created_at')
+	search_fields = ('user__username', 'note')
+
+
+@admin.register(JournalEntry)
+class JournalEntryAdmin(admin.ModelAdmin):
+	list_display = ('title', 'user', 'is_private', 'created_at', 'updated_at')
+	list_filter = ('is_private', 'created_at', 'updated_at')
+	search_fields = ('title', 'content', 'user__username')
