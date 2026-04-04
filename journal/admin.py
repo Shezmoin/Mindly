0471b7596b7eb603be568fd0b558dab31490fb49
Shezmoin <shezmoin@hotmail.com>
2026-04-04 14:53:29 +0100

    chore: align journal admin fields with current spec

--- journal/admin.py
+++ journal/admin.py
@@ -6,12 +6,10 @@ from .models import JournalEntry, MoodEntry
 @admin.register(MoodEntry)
 class MoodEntryAdmin(admin.ModelAdmin):
 	list_display = ('user', 'mood_score', 'created_at')
-	list_filter = ('mood_score', 'created_at')
-	search_fields = ('user__username', 'note')
+	list_filter = ('created_at',)
+	search_fields = ('user__username',)
 
 
 @admin.register(JournalEntry)
 class JournalEntryAdmin(admin.ModelAdmin):
-	list_display = ('title', 'user', 'is_private', 'created_at', 'updated_at')
-	list_filter = ('is_private', 'created_at', 'updated_at')
-	search_fields = ('title', 'content', 'user__username')
+	list_display = ('user', 'title', 'is_private', 'created_at')
