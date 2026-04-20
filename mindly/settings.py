2410a3c035dc0259e56f0f3075303cb375a5b82b
Shezmoin <shezmoin@hotmail.com>
2026-04-20 17:41:11 +0100

    chore: configure dj-database-url and Postgres for Heroku

--- mindly/settings.py
+++ mindly/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 from decouple import config
@@ -91,10 +92,10 @@ WSGI_APPLICATION = 'mindly.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 
